a19b41333ad7f23d4951d84e6905bfafaf5c6b56
Jade Shi <ziyushi1997@gmail.com>
2023-02-08 08:14:46 -0800

    Add new exercise

--- friends.js
+++ friends.js
@@ -1,4 +1,10 @@
-var arrays = ["Leah", "Caden", "Branden", "Carly", "Rachel"]
-console.log(arrays[2]);
-console.log(arrays[4]);
-console.log(arrays[0]);
+// var arrays = ["Leah", "Caden", "Branden", "Carly", "Rachel"]
+// console.log(arrays[2]);
+// console.log(arrays[4]);
+// console.log(arrays[0]);
+
+var funPlaces = ["Paris", "Bali", "Sweden", "Germany", "Columbia"];
+funPlaces.push("South Africa");
+console.log(funPlaces);
+
+
